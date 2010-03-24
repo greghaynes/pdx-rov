418a03116ab3052ef9f51b9b1214ebbd3959021f
Gregory Haynes <greg@greghaynes.net>
2010-03-24 14:19:59 -0700

    Query servo controller version

--- control/pyserver/main.py
+++ control/pyserver/main.py
@@ -6,37 +6,21 @@ import logging
 
 LOG_LEVEL = logging.DEBUG
 
-sc_servos = {
-	'Servo1': '/dev/ttyUSB0'
-}
-
-var_query_handlers = {}
-
-def servoSet(var, val, s):
-	print 'set var'
-
-def servoQuery(var, client, s):
-	print 'query var'
-
-def setupServos():
-	for name, path in sc_servos.iteritems():
-		try:
-			s = serial.Serial(path, 2400)
-		except serial.serialutil.SerialException:
-			errmsg = 'Invalid serial device for %s: %s' % (name, path)
-			logging.error(errmsg)
-			raise ValueError(errmsg)
-		else:
-			var_handlers[name] = (setupServos, (s,))
+class ServoController(object):
+	def __init__(self, s):
+		self.s = s
+	def version(self, q_var, client):
+		self.s.write('!SCVER?\r')
+		client.send(self.s.read(3))
+		client.send('\n')
 
 def setupLogging():
 	logging.basicConfig(level=LOG_LEVEL, format="%(asctime)s - %(levelname)s - %(name)s: %(message)s")
 
 if __name__ == '__main__':
 	setupLogging()
-	setupServos()
 	server = varserver.VarServer('', 8080)
-	for var, handler in var_query_handlers.iteritems():
-		server.var_hh.add_query_handler(var, handler[0], *handler[1])
+	sc = ServoController(serial.Serial('/dev/ttyUSB0', 2400))
+	server.var_hh.add_query_handler('SC_Version', sc.version)
 	asyncore.loop(timeout=2)
 
