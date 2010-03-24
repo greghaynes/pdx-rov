1d8d2799f815c1b1ac1fae31ecd7858580dab0b4
Gregory Haynes <greg@greghaynes.net>
2010-03-24 13:57:32 -0700

    Show more detailed logging info

--- control/pyserver/main.py
+++ control/pyserver/main.py
@@ -4,11 +4,16 @@ import socket
 import serial
 import logging
 
+LOG_LEVEL = logging.DEBUG
+
 sc_servos = {
 	'Servo1': '/dev/ttyUSB0'
 }
 
 var_query_handlers = {}
+
+def servoSet(var, val, s):
+	print 'set var'
 
 def servoQuery(var, client, s):
 	print 'query var'
@@ -18,12 +23,17 @@ def setupServos():
 		try:
 			s = serial.Serial(path, 2400)
 		except serial.serialutil.SerialException:
-			logging.error('No serial device %s' % path)
-			raise ValueError('Invalid serial device for %s: %s' % (name, path))
+			errmsg = 'Invalid serial device for %s: %s' % (name, path)
+			logging.error(errmsg)
+			raise ValueError(errmsg)
 		else:
 			var_handlers[name] = (setupServos, (s,))
 
+def setupLogging():
+	logging.basicConfig(level=LOG_LEVEL, format="%(asctime)s - %(levelname)s - %(name)s: %(message)s")
+
 if __name__ == '__main__':
+	setupLogging()
 	setupServos()
 	server = varserver.VarServer('', 8080)
 	for var, handler in var_query_handlers.iteritems():
